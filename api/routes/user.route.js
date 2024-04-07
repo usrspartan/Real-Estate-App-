1546cc095ed43dbe2f0293b1101c4047ec074483
usrspartan <mailing2anup@gmail.com>
2024-04-07 14:30:07 +0530

    create signup api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import {test} from '../controllers/user.controllers.js'
 
 const router = express.Router();
 
-router.get('/test', (req,res)=>{
-    res.json({message:"Hello World!"});
-});
+router.get('/test', test);
 
 export default router;
