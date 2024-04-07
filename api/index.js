891250c26a4d91085d38ce0a0011e8c06f039cfb
usrspartan <mailing2anup@gmail.com>
2024-04-07 13:15:09 +0530

    testing api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
@@ -12,3 +13,10 @@ const app = express();
 app.listen(3000, () => {
    console.log('Server is running on port 3000!');
   });
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  // })
+
+  app.use('/api/user', userRouter);
+  
